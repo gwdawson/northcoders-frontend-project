8bf0a785c07db63ac93ad597f5e3cb71316f7204
Gareth Dawson <gwdawson.work@gmail.com>
2022-04-19 11:14:16 +0100

    Add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
 import './styles/App.css';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1>Hello World</h1>
-      </header>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <header className='App-header'>
+              <h1>Hello World</h1>
+            </header>
+          }
+        ></Route>
+        <Route
+          path='/hi'
+          element={
+            <header className='App-header'>
+              <h1>hi?</h1>
+            </header>
+          }
+        ></Route>
+      </Routes>
     </div>
   );
 }
