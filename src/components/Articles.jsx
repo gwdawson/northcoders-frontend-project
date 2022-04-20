72d34326d2669cd8dd1592bc63a70330d6f14c88
Gareth Dawson <gwdawson.work@gmail.com>
2022-04-20 15:14:00 +0100

    Add loading status

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { getAllArticles, getArticleByTopic } from '../utils/api';
 import { useParams } from 'react-router-dom';
 import '../styles/App.css';
+import ReactLoading from 'react-loading';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMessage } from '@fortawesome/free-solid-svg-icons';
 
 export default function Articles() {
   const { topic } = useParams();
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     const makeAsync = async () => {
       let articles = undefined;
       if (topic === undefined) {
@@ -18,9 +21,14 @@ export default function Articles() {
         articles = await getArticleByTopic(topic);
       }
       setArticles(articles);
+      setLoading(false);
     };
     makeAsync();
   }, [topic]);
+
+  if (loading) {
+    return <ReactLoading className='LoadingSymbol' type='spinningBubbles' color='#ffffff' />;
+  }
 
   return (
     <div className='Articles'>
