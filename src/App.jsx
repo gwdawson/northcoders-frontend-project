1a464603c5573d3032b543ee1f2163e18e639e59
Gareth Dawson <gwdawson.work@gmail.com>
2022-04-22 14:51:49 +0100

    User must be logged in before liking or err message!

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,10 @@ import './styles/App.css';
 import { useEffect, useState } from 'react';
 import { getAllUsers } from './utils/api';
 
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// const notify = () => toast('You must be logged in to view this page');
-// <button onClick={notify}>Test!!!</button>
-// <ToastContainer />
-
 function App() {
   const [users, setUsers] = useState([]);
   const [user, setUser] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
     const makeAsync = async () => {
@@ -29,15 +24,21 @@ function App() {
       <Nav />
       {users.includes(user) && <h3 className='User'>Logged in as: {user.username}</h3>}
       {!users.includes(user) && (
-        <button className='UserLoginButton' onClick={() => setUser(users[Math.floor(Math.random() * users.length)])}>
+        <button
+          className='UserLoginButton'
+          onClick={() => {
+            setUser(users[Math.floor(Math.random() * users.length)]);
+            setLoggedIn(true);
+          }}
+        >
           LOGIN
         </button>
       )}
       <Header />
       <Routes>
-        <Route path='/' element={<Articles />}></Route>
-        <Route path='/topics/:topic' element={<Articles />}></Route>
-        <Route path='/articles/:article_id' element={<Articles />}></Route>
+        <Route path='/' element={<Articles loggedIn={loggedIn} />}></Route>
+        <Route path='/topics/:topic' element={<Articles loggedIn={loggedIn} />}></Route>
+        <Route path='/articles/:article_id' element={<Articles loggedIn={loggedIn} />}></Route>
       </Routes>
     </div>
   );
